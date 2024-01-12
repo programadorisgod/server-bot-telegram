40b456f44bd20b719850d721480d2b7fe30fda81
programadorisgod <catapiascastro@gmail.com>
2024-01-11 21:28:44 -0500

    -mbest practices endpoint command

--- src/routes/command/command.ts
+++ src/routes/command/command.ts
@@ -8,7 +8,7 @@ const path = '/api/v1/command'
 RouterCommand.get(path)
 RouterCommand.post(`${path}/:id`, addCommandBot)
 RouterCommand.put(`${path}/:id`, editCommandBot)
-RouterCommand.put(`${path}/:id/:nameCommand/:username`, editCommandAllBot)
+RouterCommand.put(`${path}/:id/:name_command/:username`, editCommandAllBot)
 RouterCommand.delete(`${path}/:id`, deleteCommandBot)
 
 export default RouterCommand
