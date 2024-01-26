038df428ea41d889680d93c82fb67d7d5f3398e8
programadorisgod <catapiascastro@gmail.com>
2024-01-26 14:10:34 -0500

    fix(update commandListDefault)

--- src/services/command/command.ts
+++ src/services/command/command.ts
@@ -29,7 +29,6 @@ const addCommand = async (
   command: ICommand
 ): Promise<Record<string, unknown> | Error> => {
   try {
-    
     const chat = await findChatById(id)
 
     if (chat instanceof Error) {
